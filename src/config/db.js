78c7620bb3193568bf8e7113a94b791ebaaadce9
yagya <yagyagoel87@gmail.com>
2024-01-22 02:40:42 +0530

    setup app.js

--- src/config/db.js
+++ src/config/db.js
@@ -6,10 +6,7 @@ const {mongodb_url} = process.env;
 
 const connectToDB = async ()=>{
     try{
-        await mongoose.connect(mongodb_url,{
-            useNewUrlParser :true,
-            useUnifiedTopology : true,
-        });
+        await mongoose.connect(mongodb_url);
     }catch(err)
     {
         console.log("unable to connect to the database"+err)
