6e2f5d7a02f925d92b692f236bca023085269841
yagya <yagyagoel87@gmail.com>
2024-01-22 02:14:43 +0530

    added db and created the main route

---
+++ src/config/app.js
@@ -0,0 +1,11 @@
+//database
+require("./config/db");
+
+const express = require("express")
+const cors= require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+module.exports =app;
---
+++ src/config/db.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+//url 
+const {mongodb_url} = process.env;
+
+const connectToDB = async ()=>{
+    try{
+        await mongoose.connect(mongodb_url,{
+            useNewUrlParser :true,
+            useUnifiedTopology : true,
+        });
+    }catch(err)
+    {
+        console.log("unable to connect to the database"+err)
+    }
+}
+connectToDB();
